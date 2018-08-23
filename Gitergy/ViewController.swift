dcf0a83bec2eb0ff977cbab16c289f6bc139e6cb
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-08-23 23:39:45 +0900

    first commit

---
+++ Gitergy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gitergy
+//
+//  Created by 이동건 on 23/08/2018.
+//  Copyright © 2018 이동건. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
