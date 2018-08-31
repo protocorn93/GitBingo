205f5cf218aed6e8a2ad13e3a85168f6b8ac03b1
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-08-31 21:03:03 +0900

    Dots Data

---
+++ Gitergy/Model/Dot.swift
@@ -0,0 +1,41 @@
+//
+//  Dot.swift
+//  Gitergy
+//
+//  Created by 이동건 on 24/08/2018.
+//  Copyright © 2018 이동건. All rights reserved.
+//
+
+import UIKit
+
+class Dot {
+    private var date: String?
+    private var rawColor: String?
+    
+    var grade: ContributionGrade? {
+        guard let color = rawColor else { return .notYet }
+        
+        if let grade = ContributionGrade(rawValue: color) {
+            return grade
+        }
+        
+        return .notYet
+    }
+    
+    var dateForOrder: Date? {
+        guard let date = date else { return nil }
+        let formatter = DateFormatter()
+        if let date = formatter.date(from: date) {
+            return date
+        }
+        
+        return nil 
+    }
+    
+    init(){}
+    
+    init(date: String, color: String) {
+        self.date = date
+        self.rawColor = color
+    }
+}
