87600c332d2b163f1a59dc8cf6c2afc89ad46a46
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-08-31 21:02:21 +0900

    Extension for UIColors

---
+++ Gitergy/Extensions/UIColorExtensions.swift
@@ -0,0 +1,15 @@
+//
+//  UIColorExtensions.swift
+//  Gitergy
+//
+//  Created by 이동건 on 24/08/2018.
+//  Copyright © 2018 이동건. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(hex: Int){
+        self.init(red: CGFloat((hex & 0xFF0000) >> 16) / 255.0, green: CGFloat((hex & 0x00FF00) >> 8) / 255.0, blue: CGFloat(hex & 0x0000FF) / 255.0, alpha: 1)
+    }
+}
