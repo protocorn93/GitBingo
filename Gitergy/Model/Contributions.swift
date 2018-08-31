9b6c131e990ef4ec9d1afae44067df677f06331c
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-08-31 21:03:20 +0900

    Contribution Data

---
+++ Gitergy/Model/Contributions.swift
@@ -0,0 +1,44 @@
+//
+//  Contributions.swift
+//  Gitergy
+//
+//  Created by 이동건 on 24/08/2018.
+//  Copyright © 2018 이동건. All rights reserved.
+//
+
+import UIKit
+
+class Contribution {
+    var dots: [Dot]
+    
+    var count: Int {
+        return dots.count
+    }
+    
+    lazy var colors: [UIColor?] = {
+        
+        var thisWeekContributions: [Dot] = []
+        let thisWeekContributedDate = dots.count % 7
+        let notYetDot = 7 - thisWeekContributedDate
+
+        for _ in 0..<thisWeekContributedDate {
+            thisWeekContributions.append(dots.removeLast())
+        }
+        
+        for _ in (0..<notYetDot) {
+            dots.append(Dot())
+        }
+        
+        for _ in 0..<thisWeekContributedDate {
+            dots.append(thisWeekContributions.removeFirst())
+        }
+        
+        let colors: [UIColor?] = dots.map {$0.grade?.color}
+        
+        return colors.reversed()
+    }()
+    
+    init(dots:[Dot]) {
+        self.dots = dots
+    }
+}
