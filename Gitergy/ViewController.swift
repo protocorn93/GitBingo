a2c06f17a36a4175282bd63bfcc9cc87b8b8af12
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-08-31 21:03:48 +0900

    ViewController Set up

--- Gitergy/ViewController.swift
+++ Gitergy/ViewController.swift
@@ -7,19 +7,48 @@
 //
 
 import UIKit
+import SVProgressHUD
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    var contribution: Contribution?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        APIService.shared.fetchContributionDots { (contributions) in
+            self.contribution = contributions
+            self.collectionView.reloadData()
+            SVProgressHUD.showSuccess(withStatus: "success")
+        }
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
     }
+}
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return contribution?.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "dotcell", for: indexPath)
+        cell.backgroundColor = contribution?.colors[indexPath.item]
+        return cell
+    }
+}
 
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let widht = self.view.frame.width / 7
+        return CGSize(width: widht, height: widht)
+    }
 }
 
