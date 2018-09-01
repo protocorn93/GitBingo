61e790f42d40ba885b64f037c908868510c6b1e2
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-09-01 23:51:49 +0900

    RefreshMode

---
+++ GitBingo/Helper/RefreshMode.swift
@@ -0,0 +1,14 @@
+//
+//  RefreshMode.swift
+//  GitBingo
+//
+//  Created by 이동건 on 01/09/2018.
+//  Copyright © 2018 이동건. All rights reserved.
+//
+
+import Foundation
+
+enum RefreshMode {
+    case pullToRefresh
+    case tapToRefresh
+}
