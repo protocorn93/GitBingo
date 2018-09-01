ddde845461dc014fb5582d075faf7716b8dd3a9d
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-09-02 00:24:15 +0900

    CollectionViewSectionHeader, CollectionViewSectionFooter

---
+++ GitBingo/View/SectionFooterView.swift
@@ -0,0 +1,13 @@
+//
+//  SectionFooterView.swift
+//  GitBingo
+//
+//  Created by 이동건 on 02/09/2018.
+//  Copyright © 2018 이동건. All rights reserved.
+//
+
+import UIKit
+
+class SectionFooterView: UICollectionReusableView {
+        
+}
---
+++ GitBingo/View/SectionHeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  SectionHeaderView.swift
+//  GitBingo
+//
+//  Created by 이동건 on 02/09/2018.
+//  Copyright © 2018 이동건. All rights reserved.
+//
+
+import UIKit
+
+class SectionHeaderView: UICollectionReusableView {
+    @IBOutlet weak var weekLabel: UILabel!
+    
+}
