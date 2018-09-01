773535271c809572cdd17cee75d3877ce5d8addc
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-09-01 18:09:45 +0900

    add Error case

--- GitBingo/Helper/GitBingoError.swift
+++ GitBingo/Helper/GitBingoError.swift
@@ -11,6 +11,7 @@ import Foundation
 enum GitBingoError: Error {
     case pageNotFound
     case unexpected
+    case idIsEmpty
     
     var description: String {
         switch self {
@@ -18,6 +19,8 @@ enum GitBingoError: Error {
             return "존재하지 않는 아이디입니다."
         case .unexpected:
             return "예상치 못한 오류가 발생하였습니다."
+        case .idIsEmpty:
+            return "아이디를 입력해주세요."
         }
     }
 }
