5d100c1a3b830cfc3f9cf3bd1f780be7a7b75158
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-09-02 00:36:42 +0900

    hide shadow navigation bar

--- GitBingo/ViewController.swift
+++ GitBingo/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.navigationBar.setValue(true, forKey: "hidesShadow")
         setupCollectionView()
         setupPresenter()
         setupRefreshControl()
