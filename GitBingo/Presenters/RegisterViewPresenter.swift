ac31f5256c9f7f38464ffbecf91f25f0e3d32563
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-09-03 17:16:58 +0900

    RegisterViewPresenter for RegisterAlertViewController

---
+++ GitBingo/Presenters/RegisterViewPresenter.swift
@@ -0,0 +1,90 @@
+//
+//  RegisterViewPresenter.swift
+//  GitBingo
+//
+//  Created by 이동건 on 03/09/2018.
+//  Copyright © 2018 이동건. All rights reserved.
+//
+
+import Foundation
+import UserNotifications
+
+protocol RegisterNotificationProtocol {
+    func showRegisterAlert(_ hasScheduledNotification: Bool, with time: String)
+    func showUnAuthorizedAlert()
+}
+
+class RegisterViewPresenter {
+    private var vc: RegisterNotificationProtocol?
+    private let center = UNUserNotificationCenter.current()
+    private var time: String
+    private var hasScheduledNotification: Bool = {
+        guard let _ = UserDefaults.standard.value(forKey: KeyIdentifier.notification.value) else { return false }
+        return true
+    }()
+    private let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = .current
+        dateFormatter.dateFormat = "HH:mm"
+        return dateFormatter
+    }()
+    
+    init() {
+        self.time = dateFormatter.string(from: Date())
+    }
+    
+    func attachView(_ vc: RegisterNotificationProtocol?) {
+        self.vc = vc
+    }
+    
+    func detatchView() {
+        self.vc = nil
+    }
+    
+    func setupTime(with date: Date) {
+        self.time = dateFormatter.string(from: date)
+    }
+    
+    func showAlert() {
+        center.getNotificationSettings { (settings) in
+            if settings.authorizationStatus == .authorized {
+                self.vc?.showRegisterAlert(self.hasScheduledNotification, with: self.time)
+            }else {
+                self.vc?.showUnAuthorizedAlert()
+            }
+        }
+    }
+    
+    func generateNotification() {
+        guard let times = pasreTime(from: time) else { return }
+        let content = UNMutableNotificationContent()
+        content.title = "Wait!"
+        content.body = "Did You Commit?🤔"
+        content.sound = UNNotificationSound.default()
+        content.badge = 1
+        
+        let userCalendar = Calendar.current
+        var components = userCalendar.dateComponents([.hour, .minute], from: Date())
+        
+        components.hour = times.hour
+        components.minute = times.minute
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: true)
+        let request = UNNotificationRequest(identifier: "GitBingo", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if ((error) != nil){
+                print("Error \(String(describing: error))")
+            }
+            
+            UserDefaults.standard.setValue(self.time, forKey: KeyIdentifier.notification.value)
+        }
+    }
+    
+    fileprivate func pasreTime(from time: String) -> (hour: Int, minute: Int)? {
+        let times = self.time.split(separator: ":").map {String($0)}
+        guard let hour = Int(times[0]) else { return nil }
+        guard let minute = Int(times[1]) else { return nil }
+        return (hour, minute)
+    }
+}
--- GitBingo/ViewControllers/RegisterAlertViewController.swift
+++ GitBingo/ViewControllers/RegisterAlertViewController.swift
@@ -7,90 +7,52 @@
 //
 
 import UIKit
-import UserNotifications
 
 class RegisterAlertViewController: UIViewController {
 
+    private var presenter: RegisterViewPresenter = RegisterViewPresenter()
     @IBOutlet weak var timePicker: UIDatePicker! {
         didSet{
             timePicker.date = Date()
-            formattingTime(from: timePicker)
         }
     }
-    
-    private var time: String = ""
-    private let center = UNUserNotificationCenter.current()
-    private var formatter: DateFormatter = {
-        let dateFormatter = DateFormatter()
-        dateFormatter.locale = .current
-        dateFormatter.dateFormat = "HH:mm"
-        return dateFormatter
-    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        presenter.attachView(self)
     }
     
     @IBAction func handleRegister(_ sender: UIButton) {
-        center.getNotificationSettings { (settings) in
-            if settings.authorizationStatus == .authorized {
-                if let _ = UserDefaults.standard.value(forKey: KeyIdentifier.notification.value) as? String {
-                    UIAlertController.showAskUpdateScheduledNotificationAlert(on: self, at: self.time, registerCompletion: { (_) in
-                        self.generateNotification()
-                        self.navigationController?.popViewController(animated: true)
-                    })
-                }
-                
-                UIAlertController.showRegisterNotificationAlert(on: self, at: self.time, registerCompletion: { (_) in
-                    self.generateNotification()
-                    self.navigationController?.popViewController(animated: true)
-                })
-            }else {
-                UIAlertController.showCheckNotificationSettingsAlert(on: self)
-            }
-        }
+        presenter.showAlert()
     }
     
     @IBAction func valueDidChanged(_ sender: UIDatePicker) {
-        formattingTime(from: sender)
+        presenter.setupTime(with: sender.date)
     }
     
-    fileprivate func formattingTime(from datePicker: UIDatePicker) {
-        let time = formatter.string(from: datePicker.date)
-        self.time = time
-    }
-    
-    fileprivate func pasreTime(from time: String) -> (hour: Int, minute: Int)? {
-        let times = self.time.split(separator: ":").map {String($0)}
-        guard let hour = Int(times[0]) else { return nil }
-        guard let minute = Int(times[1]) else { return nil }
-        return (hour, minute)
-     }
-    
-    fileprivate func generateNotification() {
-        guard let times = pasreTime(from: time) else { return }
-        let content = UNMutableNotificationContent()
-        content.title = "Wait!"
-        content.body = "Did You Commit?🤔"
-        content.sound = UNNotificationSound.default()
-        content.badge = 1
-        
-        let userCalendar = Calendar.current
-        var components = userCalendar.dateComponents([.hour, .minute], from: Date())
-        
-        components.hour = times.hour
-        components.minute = times.minute
-        
-        let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: true)
-        let request = UNNotificationRequest(identifier: "GitBingo", content: content, trigger: trigger)
-        
-        UNUserNotificationCenter.current().add(request) { (error) in
-            if ((error) != nil){
-                print("Error \(String(describing: error))")
-            }
-            
-            UserDefaults.standard.setValue(self.time, forKey: KeyIdentifier.notification.value)
-        }
+    deinit {
+        presenter.detatchView()
     }
 }
+
+extension RegisterAlertViewController: RegisterNotificationProtocol {
+    
+    func showRegisterAlert(_ hasScheduledNotification: Bool, with time: String) {
+        if hasScheduledNotification {
+            UIAlertController.showAskUpdateScheduledNotificationAlert(on: self, at: time, registerCompletion: { (_) in
+                self.presenter.generateNotification()
+                self.navigationController?.popViewController(animated: true)
+            })
+        }
+        
+        UIAlertController.showRegisterNotificationAlert(on: self, at: time, registerCompletion: { (_) in
+            self.presenter.generateNotification()
+            self.navigationController?.popViewController(animated: true)
+        })
+    }
+    
+    func showUnAuthorizedAlert() {
+        UIAlertController.showCheckNotificationSettingsAlert(on: self)
+    }
+}
+
