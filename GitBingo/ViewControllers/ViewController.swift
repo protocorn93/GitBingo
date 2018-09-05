9b0946972bab268901c7ec161d43a6f25bdfb507
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-09-06 01:46:30 +0900

    성공 Localizing 수정

--- GitBingo/ViewControllers/ViewController.swift
+++ GitBingo/ViewControllers/ViewController.swift
@@ -136,7 +136,7 @@ extension ViewController: GithubDotsRequestProtocol {
     
     func showSuccessProgressStatus() {
         UIApplication.shared.isNetworkActivityIndicatorVisible = false
-        SVProgressHUD.showSuccess(withStatus: "Success".localized)
+        SVProgressHUD.showSuccess(withStatus: "Success")
         SVProgressHUD.dismiss(withDelay: 0.6)
         
         if refreshControl.isRefreshing {
