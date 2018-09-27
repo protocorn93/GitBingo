35e495d537988b81a520dcb9306a2cb4e895a1fe
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-09-26 19:54:01 -0500

    Today Extension base UI Setup

--- GitBingo Widget/TodayViewController.swift
+++ GitBingo Widget/TodayViewController.swift
@@ -11,9 +11,28 @@ import NotificationCenter
 
 class TodayViewController: UIViewController, NCWidgetProviding {
         
+    @IBOutlet weak var todayCommitLabel: UILabel!
+    @IBOutlet weak var weekTotalLabel: UILabel!
+    @IBOutlet weak var widgetCollectionView: UICollectionView!
+    
+    private var contributions: Contribution?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        widgetCollectionView.delegate = self
+        widgetCollectionView.dataSource = self
+        load()
+    }
+    
+    private func load() {
+        if let groupUserDefaults = UserDefaults(suiteName: "group.Gitbingo"), let data = groupUserDefaults.object(forKey: KeyIdentifier.contributions.value) as? Data {
+            guard let contributions = try? PropertyListDecoder().decode(Contribution.self, from: data) else {
+                print("No contributions")
+                return
+            }
+            self.contributions = contributions
+            self.widgetCollectionView.reloadData()
+        }
     }
         
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
@@ -22,8 +41,35 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         // If an error is encountered, use NCUpdateResult.Failed
         // If there's no update required, use NCUpdateResult.NoData
         // If there's an update, use NCUpdateResult.NewData
+        self.widgetCollectionView.reloadData()
         
         completionHandler(NCUpdateResult.newData)
     }
+}
+
+extension TodayViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "widgetcell", for: indexPath)
+        cell.backgroundColor = contributions?.colors[indexPath.item]
+        return cell
+    }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+}
+
+extension TodayViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width / 7
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
 }
