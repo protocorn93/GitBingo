e9b81ce892f381893f35d704a9220b57798da643
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-10-27 16:06:29 +0900

    Refactoring APIService using Protocols

--- GitBingo/APIService/APIService.swift
+++ GitBingo/APIService/APIService.swift
@@ -9,64 +9,67 @@
 import Foundation
 import Kanna
 
-struct APIService {
+protocol APIServiceProtocol: class {
+    func fetchContributionDots(of id: String, completion: @escaping (Contribution?, GitBingoError?) ->())
+}
+
+protocol HTMLParsingProtocol: class {
+    func parse(from data: Data?) -> Contribution?
+}
+
+class Parser: HTMLParsingProtocol {
+    func parse(from data: Data?) -> Contribution? {
+        guard let data = data, let rawHTML = String(data: data, encoding: .utf8) else { return nil }
+        guard let doc = try? HTML(html: rawHTML, encoding: .utf8) else { return nil }
+        if doc.body?.content == "Not Found" { return nil }
+        
+        let dayElements = doc.css("g > .day")
+        
+        var dots: [Dot] = []
+        
+        dayElements.forEach { (day) in
+            guard let date = day["data-date"] else { return }
+            guard let color = day["fill"] else { return }
+            guard let dataCount = day["data-count"], let count = Int(dataCount) else { return }
+            let dot = Dot(date: date, color: color, count: count)
+            dots.append(dot)
+        }
+        dots.last?.isToday = true
+        return Contribution(dots: dots)
+    }
+}
+
+class APIService: APIServiceProtocol {
     //MARK: Properties
-    static let shared = APIService()
     private let session = URLSession(configuration: .default)
-    private init() {}
+    private let parser: HTMLParsingProtocol!
+    init(parser: HTMLParsingProtocol) {
+        self.parser = parser
+    }
     
     //MARK: Methods
     func fetchContributionDots(of id: String, completion: @escaping (Contribution?, GitBingoError?) -> ()) {
         if id.isEmpty {
-            completion(nil, GitBingoError.idIsEmpty)
+            completion(nil, .idIsEmpty)
             return
         }
         guard let url = URL(string: "https://github.com/users/\(id)/contributions") else {
-            completion(nil, GitBingoError.pageNotFound)
+            completion(nil, .pageNotFound)
             return
         }
         let task = self.session.dataTask(with: url) { (data, response, error) in
             if error != nil {
-                completion(nil, GitBingoError.networkError)
+                completion(nil, .networkError)
                 return
             }
             
-            guard let data = data, let rawHtml = String(data: data, encoding: .utf8) else { return }
-            
-            do {
-                let dayElements = try self.parseHTML(from: rawHtml)
-                
-                var dots: [Dot] = []
-                
-                if let dayElements = dayElements {
-                    dayElements.forEach { (day) in
-                        guard let date = day["data-date"] else { return }
-                        guard let color = day["fill"] else { return }
-                        guard let dataCount = day["data-count"], let count = Int(dataCount) else { return }
-                        let dot = Dot(date: date, color: color, count: count)
-                        dots.append(dot)
-                    }
-                    dots.last?.isToday = true
-                    completion(Contribution(dots: dots), nil)
-                }
-            } catch let err as GitBingoError {
-                completion(nil, err)
-            } catch {
-                completion(nil, GitBingoError.networkError)
+            if let contribution = self.parser.parse(from: data) {
+                completion(contribution, nil)
+            }else {
+                completion(nil, .pageNotFound)
             }
         }
         
         task.resume()
     }
-    
-    fileprivate func parseHTML(from rawHTML: String) throws -> XPathObject? {
-        guard let doc = try? HTML(html: rawHTML, encoding: .utf8) else { return nil}
-        
-        if doc.body?.content == "Not Found" {
-            throw GitBingoError.pageNotFound
-        }
-        
-        return doc.css("g > .day")
-    }
-    
 }
