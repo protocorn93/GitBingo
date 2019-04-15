9c9712907798cb22b77f441a62606add6fce9f19
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2019-04-15 23:32:50 +0900

    할로윈 색상 추가

--- GitBingo/Model/ContributionGrade.swift
+++ GitBingo/Model/ContributionGrade.swift
@@ -10,26 +10,39 @@ import UIKit
 
 enum ContributionGrade: String {
     case notYet = "#ffffff"
-    case none = "#ebedf0"
-    case less = "#c6e48b"
-    case normal = "#7bc96f"
-    case hard = "#239a3b"
-    case extreme = "#196127"
+    case noneGreen = "#ebedf0"
+    case lessGreen = "#c6e48b"
+    case normalGreen = "#7bc96f"
+    case hardGreen = "#239a3b"
+    case extremeGreen = "#196127"
+    
+    case lessHalloween = "#feec59"
+    case normalHalloween = "#fec42e"
+    case hardHalloween = "#fc9526"
+    case extremeHalloween = "#03011b"
 
     var color: UIColor {
         switch self {
         case .notYet:
             return UIColor(hex: 0xffffff)
-        case .none:
+        case .noneGreen:
             return UIColor(hex: 0xebedf0)
-        case .less:
+        case .lessGreen:
             return UIColor(hex: 0xc6e48b)
-        case .normal:
+        case .normalGreen:
             return UIColor(hex: 0x7bc96f)
-        case .hard:
+        case .hardGreen:
             return UIColor(hex: 0x239a3b)
-        case .extreme:
+        case .extremeGreen:
             return UIColor(hex: 0x196127)
+        case .lessHalloween:
+            return UIColor(hex: 0xfeec59)
+        case .normalHalloween:
+            return UIColor(hex: 0xfec42e)
+        case .hardHalloween:
+            return UIColor(hex: 0xfc9526)
+        case .extremeHalloween:
+            return UIColor(hex: 0x03011b)
         }
     }
 }
