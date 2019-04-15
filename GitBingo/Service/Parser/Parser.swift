3e56c3006c6086d50e0fa5d4d774034fe559ff1a
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2019-04-15 23:47:15 +0900

    파서 메소드 분리

--- GitBingo/Service/Parser/Parser.swift
+++ GitBingo/Service/Parser/Parser.swift
@@ -13,19 +13,24 @@ class Parser: HTMLParsingProtocol {
         guard let data = data, let rawHTML = String(data: data, encoding: .utf8) else { return nil }
         guard let doc = try? HTML(html: rawHTML, encoding: .utf8) else { return nil }
         if doc.body?.content == "Not Found" { return nil }
-
-        let dayElements = doc.css("g > .day")
-
-        var dots: [Dot] = []
-
-        dayElements.forEach { (day) in
-            guard let date = day["data-date"] else { return }
-            guard let color = day["fill"] else { return }
-            guard let dataCount = day["data-count"], let count = Int(dataCount) else { return }
-            let dot = Dot(date: date, color: color, count: count)
-            dots.append(dot)
-        }
-        dots.last?.isToday = true
+        let dots = extractDots(from: doc)
         return Contributions(dots: dots)
+    }
+    private func extractDots(from doc: HTMLDocument) -> [Dot] {
+        let dayElements = doc.css("g > .day")
+        let dots = extractDots(from: dayElements)
+        dots.last?.isToday = true
+        return dots
+    }
+    private func extractDots(from object: XPathObject) -> [Dot] {
+        var dots: [Dot] = []
+        dots = object.compactMap { extractDot(from: $0) }
+        return dots
+    }
+    private func extractDot(from element: XMLElement) -> Dot? {
+        guard let date = element["data-date"] else { return nil }
+        guard let color = element["fill"] else { return nil }
+        guard let dataCount = element["data-count"], let count = Int(dataCount) else { return nil }
+        return Dot(date: date, color: color, count: count)
     }
 }
