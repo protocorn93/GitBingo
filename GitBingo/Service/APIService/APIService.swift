f9a81104548d9d6ffb8c721321cc6968521ff4aa
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2019-06-18 21:57:43 +0900

    [feat] GitBingoContributionDotsRepository 생성

--- GitBingo/Service/APIService/APIService.swift
+++ GitBingo/Service/APIService/APIService.swift
@@ -7,9 +7,50 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 protocol APIServiceProtocol: class {
     func fetchContributionDots(of id: String, completion: @escaping (Contributions?, GitBingoError?) -> Void)
+}
+
+protocol ContributionDotsRepository {
+    func fetch(_ id: String) -> Observable<Contributions>
+}
+
+class GitBingoContributionDotsRepository: ContributionDotsRepository {
+    private let session: SessionManagerProtocol
+    private let parser: HTMLParsingProtocol
+    private var disposeBag = DisposeBag()
+    
+    init(parser: HTMLParsingProtocol, session: SessionManagerProtocol) {
+        self.parser = parser
+        self.session = session
+    }
+    
+    func fetch(_ id: String) -> Observable<Contributions> {
+        return Observable<Contributions>.create { emitter in
+            guard let url = URL(string: "https://github.com/users/\(id)/contributions") else {
+                emitter.onError(GitBingoError.pageNotFound)
+                return Disposables.create()
+            }
+
+            let task = self.session.dataTask(with: url) { (data, _, error) in
+                if error != nil {
+                    emitter.onError(GitBingoError.networkError)
+                    return
+                }
+
+                if let contributions = self.parser.parse(from: data) {
+                    emitter.onNext(contributions)
+                } else {
+                    emitter.onError(GitBingoError.pageNotFound)
+                }
+            }
+            task.resume()
+            return Disposables.create()
+        }
+    }
 }
 
 class APIService: APIServiceProtocol {
