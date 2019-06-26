d90821bc3330055074f34f222b913384c2e8e23f
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2019-06-26 21:40:59 +0900

    [feat] HomeViewModel 파일 생성

---
+++ GitBingo/ViewModel/HomeViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewModel.swift
+//  GitBingo
+//
+//  Created by 이동건 on 26/06/2019.
+//  Copyright © 2019 이동건. All rights reserved.
+//
+
+import Foundation
+
+struct HomeViewModel {
+    
+}
